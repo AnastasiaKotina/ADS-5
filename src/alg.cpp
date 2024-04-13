ed30bf112f73e317cb371369ee772ec45f3757b4
AnastasiaKotina <160050075+AnastasiaKotina@users.noreply.github.com>
2024-04-13 19:58:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,29 +6,24 @@
 int priority(char x) {
     if (x == '(') {
         return 0;
-    }
-    else if (x == ')') {
+    } else if (x == ')') {
         return 1;
-    }
-    else if (x == '+' || x == '-') {
+    } else if (x == '+' || x == '-') {
         return 2;
-    }
-    else if (x == '*' || x == '/') {
+    } else if (x == '*' || x == '/') {
         return 3;
-    }
-    else {
+    } else {
         return -1;
     }
 }
 std::string infx2pstfx(std::string inf) {
-  std::string num, num1;
+ std::string num, num1;
     TStack <char, 100> stack1;
     for (auto& x : inf) {
         int pr = priority(x);
         if (pr == -1) {
             num = num + x + ' ';
-        }
-        else {
+        } else {
             char znach = stack1.get();
 
             if ((pr == 0 || priority(znach) < pr || stack1.isEmpty()) || x == ')') {
@@ -37,28 +32,23 @@ std::string infx2pstfx(std::string inf) {
                     stack1.pop();
                     if (!stack1.isEmpty()) {
                         znach = stack1.get();
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
-
                 if (x == ')') {
                     stack1.pop();
-                }
-                else {
+                } else {
                     stack1.push(x);
                 }
-            }
-            else {
+            } else {
                 while (priority(znach) >= pr) {
                     num = num + znach + ' ';
                     stack1.pop();
 
                     if (!stack1.isEmpty()) {
                         znach = stack1.get();
-                    }
-                    else {
+                    } else {
                         break;
                     }
                 }
@@ -70,34 +60,32 @@ std::string infx2pstfx(std::string inf) {
         num = num + stack1.get() + ' ';
         stack1.pop();
     }
-    for (int i = 0; i < num.size() - 1; i++)
+    for (int i = 0; i < num.size() - 1; i++) {
         num1 += num[i];
+    }
     return num1;
 }
 
 int eval(std::string pref) {
   TStack<int, 100> stack1;
     std::string num = "";
-    for (int i = 0; i < post.size(); ++i) {
-        if (priority(post[i]) == -1) {
-            if (post[i] == ' ') {
+    for (int i = 0; i < pref.size(); ++i) {
+        if (priority(pref[i]) == -1) {
+            if (pref[i] == ' ') {
                 continue;
-            }
-            else if (isdigit(post[i + 1])) {
-                num += post[i];
+            } else if (isdigit(pref[i + 1])) {
+                num += pref[i];
                 continue;
-            }
-            else {
-                num += post[i];
+            } else {
+                num += pref[i];
                 int value = atoi(num.c_str());
                 stack1.push(value);
                 num = "";
             }
-        }
-        else {
+        } else {
             int b = stack1.pop();
             int a = stack1.pop();
-            switch (post[i]) {
+            switch (pref[i]) {
             case '+':
                 stack1.push(a + b);
                 break;
